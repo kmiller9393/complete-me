ea35fa5e876e19b0af64c63098bdac94ff85205b
Kurt Miller <kmiller9393@gmail.com>
2018-07-11 16:53:18 -0600

    Add it blocks to PrefixTrie test file

--- test/PrefixTrie-test.js
+++ test/PrefixTrie-test.js
@@ -20,6 +20,14 @@ describe('PREFIX TRIE', () => {
     expect(trie.root).to.eq(null);
   });
 
+  it('should be able to take in a word', () => {
+    // expect(trie.root).to.eq(null);
+  });
+
+  it('should increase word count ', () => {
+    expect(trie.root).to.eq(null);
+  });
+
   it('its children count increase when a new node is created', () => {
     expect(trie.count).to.eq(0);
   });
@@ -28,6 +36,10 @@ describe('PREFIX TRIE', () => {
     trie.childrenCount = 0;
     trie.insert('bob');
     trie.childrenCount = 3;
+  });
+
+  it('should keep count of how many words have been inserted', () => {
+    // expect(trie.root).to.eq(null);
   });
 
   it('should convert each character inserted to lowercase', () => {
